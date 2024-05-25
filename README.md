1d20c979c930895e4e1dfe265f2ca1e9fe500d94
zeldan <zelei.dan@gmail.com>
2024-05-25 15:21:03 +0200

    feat: quick event, skeleton of list event

--- README.md
+++ README.md
@@ -1,12 +1,67 @@
 # Google Calendar CLI - gcal
 
-Execute it locally: 
+This project is a command-line interface (CLI) tool written in Rust for managing your Google Calendar. The primary purpose is to allow users to interact with their Google Calendar directly from the terminal. Adding a new event or listing them should be very easy and quick.
+
+*Note: This project is in a very early phase, and many features are still missing. It is currently a playground project, meant for exploration and experimentation.*
+
+
+
+Happy scheduling!
+
+***
+
+## Installation
+
+1. Clone this repository
 
 ```
-cargo run -- -help
+git@github.com:zeldan/google-calendar-cli.git
 ```
+
+2. Build the project
+
+```
+cargo build --release
+```
+
+3. Run the CLI tool
+```
+./target/release/gcal --help
+```
+
+
+## Usage
+
+
+- Help
+
+```
+gcal help
+```
+
+- Quick Add Event
+
+https://developers.google.com/calendar/api/v3/reference/events/quickAdd
+
+
+```
+gcal "Retro & Demo at 16:00"
+```
+
+```
+gcal "Appointment on June 3rd 10am-10:25am"
+```
+
+- List events
+
+
+- Delete Event
+
+
 
 ## Create Application Secret
+
+TODO: 
 
 1. Enable Google Calendar
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use clap::{arg, Arg, Command};
-use google_calendar3::{api::Channel, chrono, hyper, hyper_rustls, oauth2::{self, ApplicationSecret}, CalendarHub, Error};
+use clap::{Arg, Command};
+use google_calendar3::{
+    chrono::{Utc}, hyper, hyper_rustls, oauth2::{self, ApplicationSecret}, CalendarHub
+};
 
 #[tokio::main]
 
 async fn main() {
-    let matches = Command::new("gcal")
+    let command = Command::new("gcal");
+    let matches = command
         .about("Google Calendar - CLI")
         .version("0.0.1")
         .args_conflicts_with_subcommands(true)
@@ -18,29 +21,18 @@ async fn main() {
         .subcommand(Command::new("list").about("list .."))
         .get_matches();
 
-    println!("-title used: {:?}", matches.get_one::<String>("title"));
-    println!("-date used: {:?}", matches.get_one::<String>("date"));
-
-    // Get an ApplicationSecret instance by some means. It contains the `client_id` and
-    // `client_secret`, among other things.
     let secret: oauth2::ApplicationSecret = ApplicationSecret {
         auth_uri: "https://accounts.google.com/o/oauth2/auth".to_string(),
         client_secret: "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".to_string(),
         token_uri: "https://accounts.google.com/o/oauth2/token".to_string(),
         redirect_uris: vec!["urn:ietf:wg:oauth:2.0:oob".to_string()],
-        client_id: "602236549045-3gcv7m50sp1d6vvqklimb5oaasp9ihi9.apps.googleusercontent.com".to_string(),
+        client_id: "602236549045-3gcv7m50sp1d6vvqklimb5oaasp9ihi9.apps.googleusercontent.com"
+            .to_string(),
         auth_provider_x509_cert_url: Some("https://www.googleapis.com/oauth2/v1/certs".to_string()),
         project_id: None,
         client_email: None,
         client_x509_cert_url: None,
-        
     };
-    // Instantiate the authenticator. It will choose a suitable authentication flow for you,
-    // unless you replace  `None` with the desired Flow.
-    // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
-    // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
-    // retrieve them from storage.
-    println!("------------------------------");
 
     let auth = oauth2::InstalledFlowAuthenticator::builder(
         secret,
@@ -52,8 +44,10 @@ async fn main() {
     .unwrap();
 
     let scopes = &[
-        "https://www.googleapis.com/auth/calendar.readonly", 
-        "https://www.googleapis.com/auth/calendar.events.readonly"
+        "https://www.googleapis.com/auth/calendar",
+        "https://www.googleapis.com/auth/calendar.events",
+        "https://www.googleapis.com/auth/calendar.readonly",
+        "https://www.googleapis.com/auth/calendar.events.readonly",
     ];
 
     match auth.token(scopes).await {
@@ -61,67 +55,48 @@ async fn main() {
         Err(e) => println!("error: {:?}", e),
     }
 
-    println!("-------------------------------------------");
-
     let hub = CalendarHub::new(
         hyper::Client::builder().build(
             hyper_rustls::HttpsConnectorBuilder::new()
                 .with_native_roots()
                 .https_or_http()
-                .enable_http1()
+                .enable_http2()
                 .build(),
         ),
         auth,
-        
-    ); 
+    );
 
-    // As the method needs a request, you would usually fill it with the desired information
-    // into the respective structure. Some of the parts shown here might not be applicable !
-    // Values shown here are possibly random and not representative !
-    let calendar_list = hub.calendar_list().list().doit().await;
-    match calendar_list {
-        Ok((_, calendar_list)) => {
-            println!("Calendar List:");
-            if let Some(items) = calendar_list.items {
-                for calendar in items {
-                    println!("{:?}", calendar);
+    match matches.subcommand() {
+        Some(("list", _)) => {
+            let events = hub.events().list("primary").time_min(Utc::now()).doit().await;
+            match events {
+                Ok((_, events)) => {
+                    if let Some(items) = events.items {
+                        for event in items {
+                            println!("{:?}, {:?}-{:?}, {:?}", event.summary, event.start, event.end, event.html_link);
+                        }
+                    }
+                }
+                Err(e) => println!("Error retrieving events: {:?}", e),
+            }
+        },
+        Some(("add", _)) | _ => {
+            let title = matches.get_one::<String>("title");
+            if title.is_none() {
+                return;
+            }
+            let result = hub
+                .events()
+                .quick_add("primary", title.unwrap())
+                .doit()
+                .await;
+
+            match result {
+                Ok((_, event)) => println!("Event created: {:?}", event),
+                Err(e) => {
+                    eprintln!("Error creating event: {:?}", e);
                 }
             }
-        }
-        Err(e) => println!("Error retrieving calendar list: {:?}", e),
+        },
     }
-
-    println!("-------------------------------------------");
-
-    // List calendars
-    let calendar_list = hub.calendar_list().list().doit().await;
-    match calendar_list {
-        Ok((_, calendar_list)) => {
-            println!("Calendar List:");
-            if let Some(items) = calendar_list.items {
-                for calendar in items {
-                    println!("{:?}", calendar);
-                }
-            }
-        }
-        Err(e) => println!("Error retrieving calendar list: {:?}", e),
-    }
-
-    println!("-------------------------------------------");
-
-    // Print events from the primary calendar
-    let events = hub.events().list("primary").doit().await;
-    match events {
-        Ok((_, events)) => {
-            println!("Events:");
-            if let Some(items) = events.items {
-                for event in items {
-                    println!("{:?}", event);
-                }
-            }
-        }
-        Err(e) => println!("Error retrieving events: {:?}", e),
-    }
-
-   
 }
